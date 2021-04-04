1471128fe93496c5b736acb5e2657bfb04cdc6ad
Zeshan Ghayoomi <z.ghayoomi@novafutur.com>
2021-04-05 00:56:31 +0100

    Adds wip

--- lib/src/progress_status.dart
+++ lib/src/progress_status.dart
@@ -11,14 +11,14 @@ class ProgressStatus extends StatelessWidget {
   final TextStyle _centerTextStyle;
 
   const ProgressStatus(
-      {double radius,
+      {double radius = 15.0,
       double fillValue = 0.0,
       Color fillColor = blueColor,
       Color backgroundColor = greyColor,
       bool isStrokeCapRounded = true,
       bool showCenterText = true,
       TextStyle centerTextStyle = defaultCenterTextStyle})
-      : _radius = radius,
+      : _radius = radius * 2,
         _fillValue = fillValue,
         _fillColor = fillColor,
         _backgroundColor = backgroundColor,
@@ -28,6 +28,18 @@ class ProgressStatus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Progress Status Widget');
+    return Container(
+      color: Colors.brown,
+      child: SizedBox(
+        height: _radius,
+        width: _radius,
+        child: Center(
+          child: Text(
+            '0%',
+            style: _centerTextStyle.copyWith(fontSize: _radius / 2.6),
+          ),
+        ),
+      ),
+    );
   }
 }
